07ce59e94d4cd9343f02d0eef79ee5c5989adddb
Manav Y Take <98514583+ManavTake@users.noreply.github.com>
2023-08-25 01:04:43 +0530

    Update N- Queens.c

--- N- Queens.c
+++ N- Queens.c
@@ -1,71 +1,103 @@
-#include <stdbool.h>
-#include <stdio.h>
-#define N 4
-
-void printSolution(int board[N][N])
+#include<stdio.h>
+#include<stdlib.h>
+char board[20][20];
+void printBoard(int n)
 {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if(board[i][j])
-				printf("Q ");
-			else
-				printf(". ");
-		}
-		printf("\n");
-	}
-}
-bool isSafe(int board[N][N], int row, int col)
-{
-	int i, j;
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
-
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
-
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
-
-	return true;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%c ",board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-bool solveNQUtil(int board[N][N], int col)
+int isSafe(int col, int row, int n)
 {
-	if (col >= N)
-		return true;
+    //horizontal
+    for(int j=0;j<n;j++)
+    {
+        if(board[row][j]=='Q')
+        return 0;
+    }
 
-	for (int i = 0; i < N; i++) {
-		if (isSafe(board, i, col)) {
-			board[i][col] = 1;
-			if (solveNQUtil(board, col + 1))
-				return true;
-			board[i][col] = 0; 
-		}
-	}
-	return false;
+    //vertical
+    for(int i=0;i<n;i++)
+    {
+        if(board[i][col]=='Q')
+        return 0;
+    }
+
+    //upper left
+    int r=row;
+    for(int c=col;c>=0,r>=0;c--,r--)
+    {
+        if(board[r][c]=='Q')
+        return 0;
+    }
+
+    //upper right
+    r=row;
+    for(int c=col;r>=0,c<n;r--,c++)
+    {
+        if(board[r][c]=='Q')
+        return 0;
+    }
+
+    //lower left
+    r=row;
+    for(int c=col;r<n,c>=0;r++,c--)
+    {
+        if(board[r][c]=='Q')
+        return 0;
+    }
+
+    //lower right
+    r=row;
+    for(int c=col;r<n,c<n;r++,c++)
+    {
+        if(board[r][c]=='Q')
+        return 0;
+    }
+    return 1;
 }
 
-bool solveNQ()
+void helper(int col, int n)
 {
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
-
-	if (solveNQUtil(board, 0) == false) {
-		printf("Solution does not exist");
-		return false;
-	}
-
-	printSolution(board);
-	return true;
+    if(col==n)
+    {
+        printBoard(n);
+        return;
+    }
+    for(int row=0;row<n;row++)
+    {
+        if(isSafe(col,row,n)==1)
+        {
+            board[row][col]='Q';
+            helper(col+1,n);
+            board[row][col]='.';
+        }
+    }
 }
 
-int main()
+void main()
 {
-	solveNQ();
-	return 0;
-}
+    int n;
+    printf("Enter the size of chess board:\n");
+    scanf("%d",&n);
+    if(n==2||n==3)
+    {
+        printf("No solution exists!\n");
+        exit(0);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            board[i][j]='.';
+        }
+    }
+    helper(0,n);  
+} 
